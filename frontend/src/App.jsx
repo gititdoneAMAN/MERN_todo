a6e96644677bf38d60fef5952f020b593477f85f
gititdoneAMAN <amandevbhardwaj@gmail.com>
2024-05-15 20:23:11 +0530

    added the createTODO route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Todos from "./pages/Todos";
+import CreateTodos from "./pages/CreateTodos";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,6 +16,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/todos" element={<Todos />} />
+          <Route path="/createtodos" element={<CreateTodos />} />
         </Routes>
       </BrowserRouter>
     </>
