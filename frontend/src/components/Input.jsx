272ca4771cd2d0af2fa6cd98a6f4665f8e54e312
gititdoneAMAN <amandevbhardwaj@gmail.com>
2024-05-16 12:02:48 +0530

    Added the state logic to the Input component

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 export default function Input({ placeholder, type, label }) {
+  const [data, setData] = useState("");
+
   return (
     <div>
       <div className="font-bold mb-1 text-lg">{label}</div>
@@ -7,6 +11,10 @@ export default function Input({ placeholder, type, label }) {
           className="border-solid border-2 rounded-lg border-[#000997] hover:border-red-500 w-full pt-1 pb-2 px-2"
           type={type}
           placeholder={placeholder}
+          onChange={(e) => {
+            setData(e.target.value);
+            console.log(e.target.value);
+          }}
         />
       </div>
     </div>
