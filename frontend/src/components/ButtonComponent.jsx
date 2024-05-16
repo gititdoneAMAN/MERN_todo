e9a543acd955041f3ab9b33de339ce81c1741444
gititdoneAMAN <amandevbhardwaj@gmail.com>
2024-05-16 12:40:34 +0530

    Wrote locgic to connect to backend

--- frontend/src/components/ButtonComponent.jsx
+++ frontend/src/components/ButtonComponent.jsx
@@ -1,6 +1,9 @@
-export default function ButtonComponent({ label }) {
+export default function ButtonComponent({ label, onClick }) {
   return (
-    <button className="w-full bg-black pt-1 pb-2 px-2 hover:bg-[#000557] rounded-lg text-white mb-[10px] mt-[30px]">
+    <button
+      onClick={onClick}
+      className="w-full bg-black pt-1 pb-2 px-2 hover:bg-[#000557] rounded-lg text-white mb-[5px] mt-[30px]"
+    >
       {label}
     </button>
   );
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -3,15 +3,17 @@ import ButtonComponent from "../components/ButtonComponent";
 import Heading from "../components/Heading";
 import Input from "../components/Input";
 import SubHeading from "../components/SubHeading";
+import axios from "axios";
 
 export default function Signup() {
   const [username, setUserName] = useState("");
-  const [fullName, setFullName] = useState("");
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
   const [password, setPassword] = useState("");
 
   return (
     <div className="bg-[#000517] h-screen w-screen flex justify-center items-center">
-      <div className="w-[400px] h-[480px] rounded-lg shadow-2xl shadow-[#000997] bg-white py-3 px-6">
+      <div className="w-[400px] h-[520px] rounded-lg shadow-2xl shadow-[#000997] bg-white py-3 px-6">
         <div>
           <Heading label={"Signup"} />
           <div className="mt-[25px]">
@@ -29,15 +31,28 @@ export default function Signup() {
 
             <div className="mb-2">
               <Input
-                label={"Full name"}
-                placeholder={"Aman Kumar Pandey"}
+                label={"First name"}
+                placeholder={"Aman Kumar"}
                 type={"text"}
                 onChange={(e) => {
-                  setFullName(e.target.value);
+                  setFirstName(e.target.value);
                   console.log(e.target.value);
                 }}
               />
             </div>
+
+            <div className="mb-1">
+              <Input
+                label={"Last name"}
+                placeholder={"Pandey"}
+                type={"text"}
+                onChange={(e) => {
+                  setLastName(e.target.value);
+                  console.log(e.target.value);
+                }}
+              />
+            </div>
+
             <div className="mb-1">
               <Input
                 label={"Password"}
@@ -50,7 +65,22 @@ export default function Signup() {
               />
             </div>
             <div>
-              <ButtonComponent label={"Signup"} />
+              <ButtonComponent
+                label={"Signup"}
+                onClick={async () => {
+                  const response = await axios.post(
+                    "http://localhost:3000/api/v1/user/signup",
+                    {
+                      username,
+                      firstname,
+                      lastname,
+                      password,
+                    }
+                  );
+
+                  console.log(response.data);
+                }}
+              />
             </div>
             <div>
               <SubHeading value={"Already a user? Signin"} />
